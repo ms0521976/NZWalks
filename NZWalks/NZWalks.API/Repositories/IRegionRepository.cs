ae15ec76c73022f976a7a4cf9921a19010ae25a8
kellylin <ms0521976@gmail.com>
2022-10-08 22:49:46 +0800

    End of Section3 / Controller / Swagger / DTOs /ASynchronous

---
+++ NZWalks/NZWalks.API/Repositories/IRegionRepository.cs
@@ -0,0 +1,11 @@
+﻿using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public interface IRegionRepository
+    {
+        //Asynchronous非同步執行
+       Task<IEnumerable<Region>> GetAllAsync();
+    }
+}
+
---
+++ NZWalks/NZWalks.API/Repositories/RegionRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Repositories
+{
+    public class RegionRepository : IRegionRepository
+    {
+        private readonly NZWalksDbContect nZWalksDbContext;
+
+        public RegionRepository(NZWalksDbContect nZWalksDbContext)
+        {
+            this.nZWalksDbContext = nZWalksDbContext;
+        }
+        public async Task<IEnumerable<Region>> GetAllAsync()
+        {
+            return  await nZWalksDbContext.Regions.ToListAsync();
+        }
+    }
+}
